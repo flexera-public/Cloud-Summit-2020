9f1fbabca627d71928eafe09c7a2b60369e4c430
Robert Lafleur <rlafleur@flexera.com>
2020-02-27 15:48:08 -0600

    initial commit

---
+++ Hands-On_-_Use_and_Customize_Policies_to_Automate_Cloud_Governance/LAB_CONTENT/fpt_test.bat
@@ -0,0 +1,2 @@
+REM %USERPROFILE%\Desktop\fpt\fpt %2 %1 param_email="" param_avg_cpu_upsize="-1" param_avg_cpu_downsize="-1" param_exclusion_tag_key="foo:bar" 1> fpt.out 2>&1
+%USERPROFILE%\Desktop\fpt\fpt %2 %1 param_email="" param_exclusion_tag_key="foo:bar" 1> fpt.out 2>&1
---
+++ Hands-On_-_Use_and_Customize_Policies_to_Automate_Cloud_Governance/rds_instance_cloudwatch_utilization/fpt_test.bat
@@ -0,0 +1,1 @@
+%USERPROFILE%\Desktop\fpt\fpt %2 %1 param_email="" param_avg_cpu_upsize="-1" param_avg_cpu_downsize="-1" param_exclusion_tag_key="foo:bar" 1> fpt.out 2>&1

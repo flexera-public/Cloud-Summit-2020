9b879823be58bc9d1dcce0afcabd51d64c4357e5
Robert Lafleur <6489046+rlafleur@users.noreply.github.com>
2020-03-03 14:16:57 -0800

    Policy training (#2)
    * initial commit
    
    * Update policy templates and fpt_test batch files to use credentials.
    
    * rename policy training folder

---
+++ Hands-On_-_Cloud_Governance_Policies/LAB_CONTENT/fpt_test.bat
@@ -0,0 +1,2 @@
+fpt %1 %2 param_email="" param_avg_cpu_upsize="-1" param_avg_cpu_downsize="-1" param_exclusion_tag_key="foo:bar" --credentials="auth_aws=Professional_Services_RDS_Read-Write" 1> fpt.out 2>&1
+REM %USERPROFILE%\Desktop\fpt\fpt %1 %2 param_email="" param_exclusion_tag_key="foo:bar" --credentials="auth_aws=Professional_Services_RDS_Read-Write" 1> fpt.out 2>&1
